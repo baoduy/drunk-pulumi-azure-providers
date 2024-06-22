83ff2a4e8a2c36b03c99a8e3771440cc8e51736d
Steven Hoang <hoangbaoduy@gmail.com>
2024-06-22 09:48:32 +0800

    Update VaultSecret.ts

--- src/VaultSecret.ts
+++ src/VaultSecret.ts
@@ -42,15 +42,8 @@ class VaultSecretResourceProvider
     const client = getKeyVaultBase(props.vaultName);
 
     const n = props.name ?? this.name;
-    if (!n) throw new Error("The name is not defined.");
-
     const ss = await client
-      .setSecret(
-        props.name ?? this.name,
-        props.value,
-        props.contentType,
-        props.tags,
-      )
+      .setSecret(n, props.value, props.contentType, props.tags)
       .catch(console.error);
 
     rs.id = ss!.properties.id ?? this.name;
